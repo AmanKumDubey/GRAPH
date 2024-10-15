deb1e4a1eaa0c284990081a1fabe16fbd58622f9
Aman Kumar Dubey <amankd91078@gmail.com>
2024-10-15 10:29:08 +0530

    Topological Sort in Directed Graph Using Kahn's Algorithm

---
+++ output/Graph8.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+             // <---    TOPOLOGICAL SORT IN DIRECTED GRAPH ---->
+             // <---    KAHN'S Alogithm ---->
+
+
+vector<int> TOPOSORT(vector<vector<int>>&adj, int v, int e)
+{
+
+    unordered_map<int,list<int>>adjlist;
+    for(int i=0;i<e;i++){
+        int u=adj[i][0];
+        int v=adj[i][1];
+
+        adjlist[u].push_back(v);
+    }
+
+    // find all indegree
+    vector<int>inDegree(v,0);
+    for(auto i:adjlist){
+        for(auto j:i.second){
+            inDegree[j]++;
+        }
+    }
+    // push 0' Degree in queue
+    queue<int>q;
+    for(int i=0;i<v;i++){
+        if(inDegree[i]==0){
+            q.push(i);
+        }
+    }
+
+    // do bfs
+    vector<int>ans;
+    while(!q.empty()){
+        int front=q.front();
+        q.pop();
+        ans.push_back(front);
+        for(auto neighbour:adjlist[front]){
+            inDegree[neighbour]--;
+            if(inDegree[neighbour]==0){
+                q.push(neighbour);
+            }
+        }
+    }
+    return ans;
+}
+
+int main(){
+
+    int n, m;
+    cout<<"Enter no. of nodes "<<endl;
+    cin>>n;
+    cout<<"Enter no. of vertex :"<<endl;
+    cin>>m;
+
+    vector<vector<int>>adj;
+    for(int i=0;i<m;i++){
+        int u,v;
+        cin>>u>>v;
+        adj.push_back({u,v});
+
+    }
+    vector<int>ans=TOPOSORT(adj,n,m);
+
+    cout<<"TopoLogicalSort "<<endl;
+    for(auto x:ans){
+        cout<<x<< " ";
+    }
+
+    return 0;
+}
